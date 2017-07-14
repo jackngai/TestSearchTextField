867de29bc911ab6ebdf872c17f32ee6053d65ba5
Jack Ngai <jackngai@gmail.com>
2017-07-14 10:47:14 -0400

    feat: Created Search Text Field

--- TestSearchTextField/ViewController.swift
+++ TestSearchTextField/ViewController.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import SearchTextField
 
 class ViewController: UIViewController {
-
+    
+    let myTextField = SearchTextField(frame: CGRect(x: 10, y: 100, width: 200, height: 40))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .gray
+        
+        myTextField.filterStrings(["Rad", "Ray", "Rope"])
+        myTextField.backgroundColor = .red
+        
+        setupViews()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupViews(){
+        view.addSubview(myTextField)
     }
 
 
